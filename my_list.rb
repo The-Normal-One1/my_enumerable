acd50a94d7368d951362935c6597b75b7cf35620
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-07 07:54:42 +0100

    Edit readme file and check for linters

--- my_list.rb
+++ my_list.rb
@@ -12,14 +12,14 @@ class MyList
   end
 end
 
-# list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 
-# puts list.all? { |e| e < 5 }
+puts(list.all? { |e| e < 5 })
 
-# puts list.all? { |e| e > 5 }
+puts(list.all? { |e| e > 5 })
 
-# puts list.any? { |e| e == 2 }
+puts(list.any? { |e| e == 2 })
 
-# puts list.any? { |e| e == 5 }
+puts(list.any? { |e| e == 5 })
 
-# puts list.filter(&:even?)
+puts(list.filter(&:even?))
